39df1d743fe7d0d8c289a77c16c22fd05dc7a35c
charlielukman <lukman.charlie@gmail.com>
2024-05-27 15:00:35 +0700

    Update routing in App.tsx to include a default route to /admin

--- frontend/react-batch-transaction/src/App.tsx
+++ frontend/react-batch-transaction/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import LoginPage from "./page/LoginPage";
 import RegisterPage from "./page/RegisterPage";
 import AdminPage from "./page/AdminPage";
@@ -6,12 +6,24 @@ import { ProtectedRoute } from "./components/protected-route";
 import "./App.css";
 import { AuthProvider } from "./hooks/useAuth";
 import CreateTransactionPage from "./page/CreateTransactionPage";
+import { useEffect } from "react";
+
+function DefaultRoute() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/admin');
+  }, [navigate]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<DefaultRoute />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
